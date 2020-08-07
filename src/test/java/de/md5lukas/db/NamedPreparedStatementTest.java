a8200db3999e9a14010323f03ab4dc6c04069008
Lukas Planz <lukas.planz@web.de>
2020-08-07 20:11:04 +0200

    Do not perform tests in a stupid manner

--- src/test/java/de/md5lukas/db/NamedPreparedStatementTest.java
+++ src/test/java/de/md5lukas/db/NamedPreparedStatementTest.java
@@ -1,95 +1,72 @@
 package de.md5lukas.db;
 
+import de.md5lukas.db.NamedPreparedStatement.NamedParameterParseResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NamedPreparedStatementTest {
 
     @Test
     @DisplayName("SQL not modified if no named parameters are present")
-    public void sqlNotModified() throws SQLException {
+    public void sqlNotModified() {
         String sql = "SELECT * FROM table";
 
-        Connection connection = mock(Connection.class);
+        NamedParameterParseResult nppr = NamedPreparedStatement.parseSQLString(sql);
 
-        NamedPreparedStatement nps = new NamedPreparedStatement(connection, sql);
-
-        verify(connection).prepareStatement(sql);
+        assertEquals(sql, nppr.getSubstitutedSql());
+        assertEquals(0, nppr.getIndexMappings().size());
     }
 
     @Test
     @DisplayName("SQL not modified if only a single double colon is present")
-    public void sqlNotModifiedSingleColon() throws SQLException {
+    public void sqlNotModifiedSingleColon() {
         String sql = "SELECT * FROM table WHERE id = :";
 
-        Connection connection = mock(Connection.class);
+        NamedParameterParseResult nppr = NamedPreparedStatement.parseSQLString(sql);
 
-        NamedPreparedStatement nps = new NamedPreparedStatement(connection, sql);
-
-        verify(connection).prepareStatement(sql);
+        assertEquals(sql, nppr.getSubstitutedSql());
+        assertEquals(0, nppr.getIndexMappings().size());
     }
 
     @Test
-    public void noNamedParametersShouldRemain1() throws SQLException {
-        Connection connection = mock(Connection.class);
+    public void singleParameterName() {
+        NamedParameterParseResult nppr = NamedPreparedStatement.parseSQLString("SELECT * FROM table WHERE id = :param");
 
-        NamedPreparedStatement nps = new NamedPreparedStatement(connection, "SELECT * FROM table WHERE id = :param");
-
-        verify(connection).prepareStatement("SELECT * FROM table WHERE id = ?");
+        assertEquals("SELECT * FROM table WHERE id = ?", nppr.getSubstitutedSql());
+        assertEquals(1, nppr.getIndexMappings().size());
+        assertArrayEquals(new int[] { 1 }, nppr.getIndexMappings().get("param"));
     }
 
     @Test
-    public void noNamedParametersShouldRemain2() throws SQLException {
-        Connection connection = mock(Connection.class);
+    public void singleParameterNameOccursTwice() {
+        NamedParameterParseResult nppr = NamedPreparedStatement.parseSQLString("SELECT * FROM table WHERE id = :param AND other = :param");
 
-        NamedPreparedStatement nps = new NamedPreparedStatement(connection, "SELECT * FROM table WHERE id = :param AND other = :other");
-
-        verify(connection).prepareStatement("SELECT * FROM table WHERE id = ? AND other = ?");
+        assertEquals("SELECT * FROM table WHERE id = ? AND other = ?", nppr.getSubstitutedSql());
+        assertEquals(1, nppr.getIndexMappings().size());
+        assertArrayEquals(new int[] { 1, 2 }, nppr.getIndexMappings().get("param"));
     }
 
     @Test
-    public void singleParameterName() throws SQLException {
-        Connection connection = mock(Connection.class);
+    public void twoParameterNames() {
+        NamedParameterParseResult nppr = NamedPreparedStatement.parseSQLString("SELECT * FROM table WHERE id = :param AND other = :other");
 
-        NamedPreparedStatement nps = new NamedPreparedStatement(connection, "SELECT * FROM table WHERE id = :param");
-
-        assertArrayEquals(new int[] { 1 }, nps.getIndexes("param"));
+        assertEquals("SELECT * FROM table WHERE id = ? AND other = ?", nppr.getSubstitutedSql());
+        assertEquals(2, nppr.getIndexMappings().size());
+        assertArrayEquals(new int[] { 1 }, nppr.getIndexMappings().get("param"));
+        assertArrayEquals(new int[] { 2 }, nppr.getIndexMappings().get("other"));
     }
 
     @Test
-    public void singleParameterNameOccursTwice() throws SQLException {
-        Connection connection = mock(Connection.class);
+    public void twoParameterNamesOccursTwice() {
+        NamedParameterParseResult nppr = NamedPreparedStatement
+                .parseSQLString("SELECT * FROM table WHERE id = :param AND other = :other AND o2 = :param AND o3 = :other");
 
-        NamedPreparedStatement nps = new NamedPreparedStatement(connection, "SELECT * FROM table WHERE id = :param AND other = :param");
-
-        assertArrayEquals(new int[] { 1, 2 }, nps.getIndexes("param"));
-    }
-
-    @Test
-    public void twoParameterNames() throws SQLException {
-        Connection connection = mock(Connection.class);
-
-        NamedPreparedStatement nps = new NamedPreparedStatement(connection, "SELECT * FROM table WHERE id = :param AND other = :other");
-
-        assertArrayEquals(new int[] { 1 }, nps.getIndexes("param"));
-        assertArrayEquals(new int[] { 2 }, nps.getIndexes("other"));
-    }
-
-    @Test
-    public void twoParameterNamesOccursTwice() throws SQLException {
-        Connection connection = mock(Connection.class);
-
-        NamedPreparedStatement nps = new NamedPreparedStatement(connection,
-                "SELECT * FROM table WHERE id = :param AND other = :other AND o2 = :param AND o3 = :other");
-
-        assertArrayEquals(new int[] { 1, 3 }, nps.getIndexes("param"));
-        assertArrayEquals(new int[] { 2, 4 }, nps.getIndexes("other"));
+        assertEquals("SELECT * FROM table WHERE id = ? AND other = ? AND o2 = ? AND o3 = ?", nppr.getSubstitutedSql());
+        assertEquals(2, nppr.getIndexMappings().size());
+        assertArrayEquals(new int[] { 1, 3 }, nppr.getIndexMappings().get("param"));
+        assertArrayEquals(new int[] { 2, 4 }, nppr.getIndexMappings().get("other"));
     }
 }
